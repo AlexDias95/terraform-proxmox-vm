7c639c95676df09a5a037807876c9514285e5dc4
Alexandre Dias <alex.m.a.dias95@gmail.com>
2021-12-29 19:38:59 +0000

    fix some other fields

--- 02-vm.tf
+++ 02-vm.tf
@@ -2,16 +2,12 @@ resource "proxmox_vm_qemu" "proxmox_vm" {
   count             = 1
   name              = var.vm_name
   target_node       = "pve01"
-clone             = var.vm_template
-os_type           = "cloud-init"
+  clone             = var.vm_template
   cores             = var.vm_cpu
   sockets           = "1"
   cpu               = "host"
   memory            = var.vm_ram
-  scsihw            = "virtio-scsi-pci"
-  bootdisk          = "scsi0"
 disk {
-    id              = 0
     size            = var.vm_disk
     type            = "scsi"
     storage         = "local-lvm"
