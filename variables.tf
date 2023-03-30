ab34702cf9b6c5e7bdd356a44dafb94d6f97e407
Alexandre Dias <47275399+AlexDias95@users.noreply.github.com>
2023-03-30 18:21:52 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,39 @@
 variable "proxmox-ip" {
   type = string
+  default = ""
 }
 
 variable "proxmox-user" {
   type = string
+  default = ""
 }
 
 variable "proxmox-password" {
   type = string
+  default = ""
 }
 
 variable "vm-name" {
   type = string
+  default = ""
 }
 
 variable "vm-template" {
   type = string
+  default = ""
 }
 
 variable "vm-cpu" {
   type = string
+  default = ""
 }
 
 variable "vm-ram" {
   type = string
+  default = ""
 }
 
 variable "vm-disk" {
   type = string
+  default = ""
 }
