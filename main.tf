3514027d09c5d265829ff1c9899ff6c2dc6c81bf
Alexandre Moura Antunes Luís Dias <alexandre.dias@ocean.ibm.com>
2023-06-21 16:08:49 +0100

    Debug

--- main.tf
+++ main.tf
@@ -1,45 +1,7 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "2.9.14"
-    }
-  }
-}
-
-provider "proxmox" {
-    pm_api_url = "https://${var.proxmox-ip}:8006/api2/json"
-    pm_user = "${var.proxmox-user}@pam"
-    pm_password = var.proxmox-password
-    pm_tls_insecure = "true"
-    pm_log_enable = true
-    pm_log_file = "terraform-plugin-proxmox.log"
-     pm_debug = true
-    pm_log_levels = {
-      _default = "debug"
-      _capturelog = ""
-    }
-}
-
-
-
-resource "proxmox_vm_qemu" "proxmox_vm" {
-  count             = 1
-  name              = var.vm-name
-  bios              = "ovmf"
-  target_node       = "pve01"
-  clone             = var.vm-template
-  cores             = var.vm-cpu
-  sockets           = "1"
-  cpu               = "host"
-  memory            = var.vm-ram
-disk {
-    size            = var.vm-disk
-    type            = "scsi"
-    storage         = "local-fastlvm"
-  }
-network {
-    model           = "virtio"
-    bridge          = "vmbr1"
+resource "null_resource" "test" {
+    provisioner "local-exec" {
+    command = <<EOT
+      echo "hello world"
+    EOT
   }
 }
